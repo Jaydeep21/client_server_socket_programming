f3c0dbc409331230ac290c4409b88341d684a34a
Jaydeep <jaydeepdharamsey21@gmail.com>
2022-12-11 23:36:43 -0500

    added comments

--- client.c
+++ client.c
@@ -11,68 +11,71 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-#define SIZE 1024
+#define SIZE 1024 //defaulting string size
 
 int main(int argc, char *argv[]){
-    char *ip = "127.0.0.1";
-    char *ip1 = "127.0.0.1";
-    int port = htons(8080);
-    int port2 = htons(8081);
-    int e;
-    int sockfd;
-    int sockfd2;
-    struct sockaddr_in server_addr;
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(sockfd < 0) {
+    char *ip = "127.0.0.1"; //setting ip for serverA
+    char *ip1 = "127.0.0.1"; //setting ip for serverB
+    int port = htons(8080); //setting port for serverA
+    int port2 = htons(8081); //setting port for serverB
+    int e; //to store connection op
+    int sockfd; //to initialize socketA
+    int sockfd2; //to initialize socketB
+    struct sockaddr_in server_addr; //to store connection address for server A
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); //creating socketA
+    if(sockfd < 0) { //incase of any problem while initializing socket
         perror("Error in socket");
         exit(1);
     }
-    memset(&server_addr, 0, sizeof(server_addr)); 
-    server_addr.sin_family = AF_INET;
+    memset(&server_addr, 0, sizeof(server_addr));  //clear pervious server memory
+    //setting up address for serverA
+    server_addr.sin_family = AF_INET; 
     server_addr.sin_port = port;
     server_addr.sin_addr.s_addr = inet_addr(ip);
 
+    //connect to serverA
     e = connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
-    if(e == -1) {
+    if(e == -1) { //if we get error while initializing socketA
         perror("Error in socket");
         exit(1);
     }
 	printf("Connected to Server.\n");
     
-    int n;
-    char input[SIZE];
-    char buffer[SIZE];
+    int n; //to receive the lenght of input message
+    char input[SIZE]; //to get input value
+    char buffer[SIZE]; //to get what acknowledgement from server  
     
-    n = recv(sockfd, buffer, sizeof(buffer), 0);
-    if (strncmp(buffer, "NA", 2) == 0){
-        close(sockfd);
-        sockfd = socket(AF_INET, SOCK_STREAM, 0);
-        struct sockaddr_in server_addr2;
-        memset(&server_addr2, 0, sizeof(server_addr)); 
-        server_addr2.sin_family = AF_INET;
+    n = recv(sockfd, buffer, sizeof(buffer), 0); //receive the acknowledgement from server in buffer variable
+    if (strncmp(buffer, "NA", 2) == 0){ //incase of negative ack connect to server B
+        close(sockfd); //close previous socket
+        sockfd = socket(AF_INET, SOCK_STREAM, 0); //creating socketB
+        struct sockaddr_in server_addr2; //to store connection address for server B
+        memset(&server_addr2, 0, sizeof(server_addr)); //clear pervious server memory
+        //setting up address for serverB
+        server_addr2.sin_family = AF_INET; 
         server_addr2.sin_port = port2;
         server_addr2.sin_addr.s_addr = inet_addr(ip1);
-        e = connect(sockfd, (struct sockaddr*)&server_addr2, sizeof(server_addr2));
-        if(e == -1) {
+        e = connect(sockfd, (struct sockaddr*)&server_addr2, sizeof(server_addr2)); //connect to serverB
+        if(e == -1) { //if we get error while initializing socketB
             perror("Error in socket");
             exit(1);
         }
-    recv(sockfd, buffer, sizeof(buffer), 0);
+    recv(sockfd, buffer, sizeof(buffer), 0); //receive the message from server in buffer variable
     }
-    while (1) {
-        memset(buffer, 0, SIZE);
-        memset(input, 0, SIZE);
+    while (1) { //until client exits the server but stating quit or by SIG
+        memset(buffer, 0, SIZE); //clear memory for buffer 
+        memset(input, 0, SIZE); //clear memory for input string
         printf("chat> ");
-        fgets(input, sizeof(input), stdin);
-        write(sockfd, input, sizeof(input));
-        if (strncmp(input, "quit", 4) == 0)
+        fgets(input, sizeof(input), stdin); //take the input from the terminal
+        write(sockfd, input, sizeof(input)); //write the output to server
+        if (strncmp(input, "quit", 4) == 0) //if quit is passed as input exit and close socket
         {
             close(sockfd);
             exit(0);
         }
-        sleep(1);
-        recv(sockfd, buffer, sizeof(buffer), 0);
-        printf("%s", buffer);
+        sleep(1); //to make sure that the quit message is read by server 
+        recv(sockfd, buffer, sizeof(buffer), 0); //get response from server in normal cases
+        printf("%s", buffer); //print the response
     }
     return 1;
 }
--- server.c
+++ server.c
@@ -11,23 +11,23 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-#define SIZE 1024
+#define SIZE 1024 //defaulting string size
 
 int serviceClient(int new_sock){
-    char opt[SIZE];
-    int saved_stdout = dup(1);
-    dup2(new_sock, 0);
-    dup2(new_sock, 1);
-    dup2(new_sock, 2);
-    char word[SIZE];
-    while(1){
-        bzero(word, SIZE);
-        bzero(opt, SIZE);
-        if (read(0, word, sizeof(word))<0){
-            printf("read() error\n");
+    char opt[SIZE]; //to store output generated
+    int saved_stdout = dup(1); // this will be used to quit the process and close server
+    dup2(new_sock, 0); //set input stream to new socket descriptor from STDIN
+    dup2(new_sock, 1); //set output stream to new socket descriptor from STDOUT
+    dup2(new_sock, 2); //set error stream to new socket descriptor from STDERR
+    char word[SIZE]; //to store input
+    while(1){ //while client wants to execute command
+        bzero(word, SIZE); //clearing input after every iteration
+        bzero(opt, SIZE); //clearing output after every iteration
+        if (read(0, word, sizeof(word))<0){ //reading message from default input(which is now socket ip)
+            printf("read() error\n"); 
             exit(3); 
         }
-        if (strncmp(word, "quit", 4) == 0)
+        if (strncmp(word, "quit", 4) == 0) //if passed string is quit close the socket and kill child process
         {
             dup2(saved_stdout, 1);
             printf("Client Disconnected\n\n");
@@ -35,41 +35,43 @@ int serviceClient(int new_sock){
             exit(0);
         }
         printf("Output for the command %s\n", word);
-        system(word);
+        system(word); //execute the command request from client
     }
 }
 
 int main(){
-    char *ip = "127.0.0.1";
-    char *ip1 = "127.0.0.1";
-    int portA = htons(8080);
-    int portB = htons(8081);
-    int eA, eB, count = 0;
-    int flag = 0;
-    int sockfdA, sockfdB, new_sock;
-    struct sockaddr_in server_addr, new_addr;
-    socklen_t addr_size;
-    char buffer[SIZE];
+    char *ip = "127.0.0.1"; //setting ip address for serverA
+    char *ip1 = "127.0.0.1"; //setting ip address for serverB
+    int portA = htons(8080); //setting port for serverA
+    int portB = htons(8081); //setting port for serverB
+    int eA, eB, count = 0; //initializing eA and eB to store binding output & count for load balancing
+    int flag = 0; // flag to select which server client should connect to 
+    int sockfdA, sockfdB, new_sock; //socket for server A and B along with file descriptor of connected socket
+    struct sockaddr_in server_addr, new_addr; //connection struct 
+    socklen_t addr_size; //address length
 
-    sockfdA = socket(AF_INET, SOCK_STREAM, 0);
-    sockfdB = socket(AF_INET, SOCK_STREAM, 0);
-    if(sockfdA < 0) {
+    sockfdA = socket(AF_INET, SOCK_STREAM, 0); //socket for serverA
+    sockfdB = socket(AF_INET, SOCK_STREAM, 0); //socket for serverB
+    if(sockfdA < 0) { //error in socket A initialization
         perror("Error in socketA");
         exit(1);
     }
-    if(sockfdA < 0) {
+    if(sockfdA < 0) { //error in socket B initialization
         perror("Error in socketB");
         exit(1);
     }
     printf("Server Socket Created Successfully.\n");
-
+    //setting connection params
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = portA;
     server_addr.sin_addr.s_addr = inet_addr(ip);
 
+    //binding serverA
     eA = bind(sockfdA, (struct sockaddr*)&server_addr, sizeof(server_addr));
 
+    //if binding serverA fails connect to server B
     if(eA < 0) {
+        //change ip and port as per serve rB
         server_addr.sin_port = portB;
         server_addr.sin_addr.s_addr = inet_addr(ip1);
         flag = 1;
@@ -86,13 +88,15 @@ int main(){
         printf("Binding successful with server A.\n");
         
     }
-    if(listen(sockfdA, 20) == 0){
+    //setting up socketA listening capacity
+    if(listen(sockfdA, 200) == 0){
         }   
     else{
         perror("Error in listening A");
         exit(1);
     }
-    if(listen(sockfdB, 20) == 0){
+    //setting up socketB listening capacity
+    if(listen(sockfdB, 200) == 0){
     }   
     else{
         perror("Error in listening B");
@@ -100,50 +104,50 @@ int main(){
     }
     addr_size = sizeof(new_addr);
 
-    if (flag==0){
-        while(1)
+    if (flag==0){ //flag 0 indicates always connect to serverA and 1 indicates always connect to serverB 
+        while(1) //always keep looking for clients
         {   
-            count++;
-            if(count>=1 && count<=5) {
-                new_sock = accept(sockfdA, (struct sockaddr*)&new_addr, &addr_size);
-                send(new_sock, "Welcome", sizeof("Welcome"), 0);
+            count++; //to count number of clients connected
+            if(count>=1 && count<=5) { //if current count is between 1 to 5 connect to server A
+                new_sock = accept(sockfdA, (struct sockaddr*)&new_addr, &addr_size); //accept the connection
+                send(new_sock, "Welcome", sizeof("Welcome"), 0); //send welcome message(positive acknowledgement)
             }
-            else if(count>=6 && count<=10){
-                new_sock = accept(sockfdA, (struct sockaddr*)&new_addr, &addr_size);
-                send(new_sock, "NA", sizeof("NA"), 0);
-                sleep(1);
-                close(new_sock);
+            else if(count>=6 && count<=10){//if current count is between 6 to 10 connect to server A
+                new_sock = accept(sockfdA, (struct sockaddr*)&new_addr, &addr_size); //accpet the connection
+                send(new_sock, "NA", sizeof("NA"), 0); //send NA negative acknowledgement
+                sleep(1); //sleep for a second
+                close(new_sock); //close the connection
                 printf("Transferred to Server B\n\n");
-                continue;
+                continue; //keep looking for new clients
             }
-            else if(count % 2 == 1){
-                new_sock = accept(sockfdA, (struct sockaddr*)&new_addr, &addr_size);
-                send(new_sock, "Welcome", sizeof("Welcome"), 0);
+            else if(count % 2 == 1){//even number of client connection after 10(eg 11,13,15, ...)
+                new_sock = accept(sockfdA, (struct sockaddr*)&new_addr, &addr_size);//accept the connection if its nth odd number of connection
+                send(new_sock, "Welcome", sizeof("Welcome"), 0); //send welcome message(positive acknowledgement)
             }
-            else{
-                new_sock = accept(sockfdA, (struct sockaddr*)&new_addr, &addr_size);
-                send(new_sock, "NA", sizeof("NA"), 0);
-                sleep(1);
-                close(new_sock);
+            else{ //even number of client connection after 10 (eg 12,14,16, ...)
+                new_sock = accept(sockfdA, (struct sockaddr*)&new_addr, &addr_size); //accept the connection
+                send(new_sock, "NA", sizeof("NA"), 0); //send NA negative acknowledgement
+                sleep(1); //sleep for a second
+                close(new_sock);  //close the connection
                 printf("Transferred to Server B\n\n");
-                continue;
+                continue; //keep looking for new clients
             }
             printf("\nReceived a Client\n");
-            if(!fork()){
+            if(!fork()){ //let child process handle the client and parent goes back to look for new client
                 serviceClient(new_sock);
             }
-            close(new_sock);
+            close(new_sock); //close the socket in parent process
         }  
         }
     else{
-        while(1){
-            new_sock = accept(sockfdB, (struct sockaddr*)&new_addr, &addr_size);
-            send(new_sock, "Welcome", sizeof("Welcome"), 0);
+        while(1){ //serverB is running and looking for client
+            new_sock = accept(sockfdB, (struct sockaddr*)&new_addr, &addr_size); //accept any connection bcoz server A has evaluated the request
+            send(new_sock, "Welcome", sizeof("Welcome"), 0); //send welcome message(positive acknowledgement)
             printf("\nReceived a Client\n");
-            if(!fork()){
+            if(!fork()){ //let child process handle the client and parent goes back to look for new client
                 serviceClient(new_sock);
             }
-            close(new_sock);
+            close(new_sock); //close the socket in parent process
         }
     }
 }
